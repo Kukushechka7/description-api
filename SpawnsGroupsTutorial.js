e6490fa9153e2aa5efcdf7b45b620b37e77961d5
Kukushechka7 <123239021+Kukushechka7@users.noreply.github.com>
2023-04-19 16:56:18 +0700

    Delete SpawnsGroupsTutorial.js

--- SpawnsGroupsTutorial.js
+++
@@ -1,6 +0,0 @@
-// создаём команды
-Teams.Add("Blue", "Teams/Blue", { b: 1 });
-Teams.Add("Red", "Teams/Red", { r: 1 });
-// делаем настройки дефолтных групп спавнов
-Teams.Get("Blue").Spawns.SpawnPointsGroups.Add(1);
-Teams.Get("Red").Spawns.SpawnPointsGroups.Add(2);
